2cbf5cea08dc3986c103d405126c62f05fe52632
Strauberg <StefanStrauberg@gmail.com>
2025-02-21 11:25:35 +0300

    Added EF, created AppDbContext, Migrate all projects to DotNet 9.0

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,6 @@
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+  public DbSet<Activity> Activities { get; set; }
+}
