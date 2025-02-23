16a9161e63fda72272804f481ee4d0f926ebe6a1
Strauberg <StefanStrauberg@gmail.com>
2025-02-23 15:40:33 +0300

    Added activity list and details

--- client/src/features/activities/Dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/Dashboard/ActivityDashboard.tsx
@@ -1,5 +1,7 @@
-import { Grid2, List, ListItem, ListItemText } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import { Activity } from "../../../lib/types/Activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
 
 type Props = {
   activities: Activity[];
@@ -7,15 +9,12 @@ type Props = {
 
 export default function ActivityDashboard({ activities }: Props) {
   return (
-    <Grid2 container>
-      <Grid2 size={9}>
-        <List>
-          {activities.map((activity) => (
-            <ListItem key={activity.id}>
-              <ListItemText>{activity.title}</ListItemText>
-            </ListItem>
-          ))}
-        </List>
+    <Grid2 container spacing={3}>
+      <Grid2 size={7}>
+        <ActivityList activities={activities} />
+      </Grid2>
+      <Grid2 size={5}>
+        {activities[0] && <ActivityDetails activity={activities[0]} />}
       </Grid2>
     </Grid2>
   );
