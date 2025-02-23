f249021e89104ccc0cc16daa978e54db965be424
Strauberg <StefanStrauberg@gmail.com>
2025-02-23 10:14:55 +0300

    Slightly changed folder structure, added navbar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { Activity } from "./lib/types/Activity";
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import { Activity } from "../../lib/types/Activity";
+import { CssBaseline, List, ListItem, ListItemText } from "@mui/material";
 import axios from "axios";
+import NavBar from "./NavBar";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -14,7 +15,8 @@ function App() {
 
   return (
     <>
-      <Typography variant="h3">Reactivities</Typography>
+      <CssBaseline />
+      <NavBar />
       <List>
         {activities.map((activity) => (
           <ListItem key={activity.id}>
