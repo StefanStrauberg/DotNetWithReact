a7059eef99d48c92eb0b787b328edefb371108fd
Strauberg <StefanStrauberg@gmail.com>
2025-03-13 16:38:37 +0300

    Added loading state

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -3,14 +3,18 @@ import {
   AppBar,
   Box,
   Container,
+  LinearProgress,
   MenuItem,
   Toolbar,
   Typography,
 } from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLinks from "../shared/components/MenuItemLinks";
+import { useStore } from "../../lib/hooks/useStore";
+import { Observer } from "mobx-react-lite";
 
 export default function NavBar() {
+  const { uiStore } = useStore();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -18,6 +22,7 @@ export default function NavBar() {
         sx={{
           backgroundImage:
             "linear-gradient(35deg, #182a73 0%, #218aae 69%, #20a7ac)",
+          position: "relative",
         }}
       >
         <Container maxWidth="xl">
@@ -44,6 +49,22 @@ export default function NavBar() {
             <MenuItem>User menu</MenuItem>
           </Toolbar>
         </Container>
+        <Observer>
+          {() =>
+            uiStore.isLoading ? (
+              <LinearProgress
+                color="secondary"
+                sx={{
+                  position: "absolute",
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  height: 4,
+                }}
+              />
+            ) : null
+          }
+        </Observer>
       </AppBar>
     </Box>
   );
